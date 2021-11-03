38730c16b4fa2c6e48c69265244ef943867279ac
MaiHon <mai.hong0924@gmail.com>
2021-11-03 21:26:38 +0900

    add evaluation scripts

---
+++ src/evals/__init__.py
@@ -0,0 +1,5 @@
+from .calc_score import Scoring, cal_text_score
+from .ic13 import calc_icdar_det_metrics
+from .deteval import calc_deteval_metrics
+from .iou import DetectionIoUEvaluator
+from .quad_metric import QuadMetric
