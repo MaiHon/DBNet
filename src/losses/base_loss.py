ac9fcf5b87621f764d1e31cb6dbe443ac6ca8d1f
MaiHon <mai.hong0924@gmail.com>
2021-11-04 15:26:35 +0900

    add base loss for textfusenet

--- src/losses/base_loss.py
+++ src/losses/base_loss.py
@@ -101,3 +101,174 @@ class MaskL1Loss(nn.Module):
     def forward(self, pred: torch.Tensor, gt, mask):
         loss = (torch.abs(pred - gt) * mask).sum() / (mask.sum() + self.eps)
         return loss
+
+
+def decode(deltas, bbox_reg_weights, bbox_xform_clip=math.log(1000. / 16)):
+    wx, wy, ww, wh = bbox_reg_weights
+    dx = deltas[:, 0::4] / wx
+    dy = deltas[:, 1::4] / wy
+    dw = deltas[:, 2::4] / ww
+    dh = deltas[:, 3::4] / wh
+
+    dw = torch.clamp(dw, max=bbox_xform_clip)
+    dh = torch.clamp(dh, max=bbox_xform_clip)
+
+    pred_ctr_x = dx
+    pred_ctr_y = dy
+    pred_w = torch.exp(dw)
+    pred_h = torch.exp(dh)
+
+    x1 = pred_ctr_x - 0.5 * pred_w
+    y1 = pred_ctr_y - 0.5 * pred_h
+    x2 = pred_ctr_x + 0.5 * pred_w
+    y2 = pred_ctr_y + 0.5 * pred_h
+
+    return x1.view(-1), y1.view(-1), x2.view(-1), y2.view(-1)
+
+
+def smooth_l1_loss(bbox_pred, bbox_targets, beta=1.0):
+    """
+    SmoothL1(x) = 0.5 * x^2 / beta      if |x| < beta
+                  |x| - 0.5 * beta      otherwise.
+    1 / N * sum_i alpha_out[i] * SmoothL1(alpha_in[i] * (y_hat[i] - y[i])).
+    N is the number of batch elements in the input predictions
+    """
+    in_box_diff = bbox_pred - bbox_targets
+    abs_in_box_diff = torch.abs(in_box_diff)
+
+    smoothL1_sign = (abs_in_box_diff < beta).detach().float()
+
+    out_loss_box = smoothL1_sign * 0.5 * torch.pow(in_box_diff, 2) / beta + \
+                  (1 - smoothL1_sign) * (abs_in_box_diff - (0.5 * beta))
+
+    loss_box = out_loss_box
+
+    N = loss_box.size(0)  # batch size
+    loss_box = loss_box.view(-1).sum(0) / N
+
+    return loss_box
+
+
+def generalized_iou(output, target, bbox_reg_weights):
+    x1, y1, x2, y2 = decode(output, bbox_reg_weights)
+    x1g, y1g, x2g, y2g = decode(target, bbox_reg_weights)
+
+    x2 = torch.max(x1, x2)
+    y2 = torch.max(y1, y2)
+
+    xkis1 = torch.max(x1, x1g)
+    ykis1 = torch.max(y1, y1g)
+    xkis2 = torch.min(x2, x2g)
+    ykis2 = torch.min(y2, y2g)
+
+    xc1 = torch.min(x1, x1g)
+    yc1 = torch.min(y1, y1g)
+    xc2 = torch.max(x2, x2g)
+    yc2 = torch.max(y2, y2g)
+
+    intsctk = torch.zeros(x1.size()).to(output)
+    mask = (ykis2 > ykis1) * (xkis2 > xkis1)
+    intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
+    unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk + 1e-7
+    iouk = intsctk / unionk
+
+    area_c = (xc2 - xc1) * (yc2 - yc1) + 1e-7
+    giouk = iouk - ((area_c - unionk) / area_c)
+    iouk = (1 - iouk).sum(0) / output.size(0)
+    giouk = (1 - giouk).sum(0) / output.size(0)
+
+    return iouk, giouk
+
+
+def distance_iou(output, target, bbox_reg_weights):
+    x1, y1, x2, y2 = decode(output, bbox_reg_weights)
+    x1g, y1g, x2g, y2g = decode(target, bbox_reg_weights)
+
+    x2 = torch.max(x1, x2)
+    y2 = torch.max(y1, y2)
+
+    x_p = (x2 + x1) / 2
+    y_p = (y2 + y1) / 2
+    x_g = (x1g + x2g) / 2
+    y_g = (y1g + y2g) / 2
+
+    xkis1 = torch.max(x1, x1g)
+    ykis1 = torch.max(y1, y1g)
+    xkis2 = torch.min(x2, x2g)
+    ykis2 = torch.min(y2, y2g)
+
+    xc1 = torch.min(x1, x1g)
+    yc1 = torch.min(y1, y1g)
+    xc2 = torch.max(x2, x2g)
+    yc2 = torch.max(y2, y2g)
+
+    intsctk = torch.zeros(x1.size()).to(output)
+    mask = (ykis2 > ykis1) * (xkis2 > xkis1)
+    intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
+    unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk + 1e-7
+    iouk = intsctk / unionk
+
+    c = ((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) + 1e-7
+    d = ((x_p - x_g) ** 2) + ((y_p - y_g) ** 2)
+    u = d / c
+    diouk = iouk - u
+    iouk = (1 - iouk).sum(0) / output.size(0)
+    diouk = (1 - diouk).sum(0) / output.size(0)
+
+    return iouk, diouk
+
+
+def complete_iou(output, target, bbox_reg_weights):
+    x1, y1, x2, y2 = decode(output, bbox_reg_weights)
+    x1g, y1g, x2g, y2g = decode(target, bbox_reg_weights)
+
+    x2 = torch.max(x1, x2)
+    y2 = torch.max(y1, y2)
+
+    w_pred = x2 - x1
+    h_pred = y2 - y1
+    w_gt = x2g - x1g
+    h_gt = y2g - y1g
+
+    x_center = (x2 + x1) / 2
+    y_center = (y2 + y1) / 2
+    x_center_g = (x1g + x2g) / 2
+    y_center_g = (y1g + y2g) / 2
+
+    xkis1 = torch.max(x1, x1g)
+    ykis1 = torch.max(y1, y1g)
+    xkis2 = torch.min(x2, x2g)
+    ykis2 = torch.min(y2, y2g)
+
+    xc1 = torch.min(x1, x1g)
+    yc1 = torch.min(y1, y1g)
+    xc2 = torch.max(x2, x2g)
+    yc2 = torch.max(y2, y2g)
+
+    intsctk = torch.zeros(x1.size()).to(output)
+    mask = (ykis2 > ykis1) * (xkis2 > xkis1)
+    intsctk[mask] = (xkis2[mask] - xkis1[mask]) * (ykis2[mask] - ykis1[mask])
+    unionk = (x2 - x1) * (y2 - y1) + (x2g - x1g) * (y2g - y1g) - intsctk + 1e-7
+    iouk = intsctk / unionk
+
+    c = ((xc2 - xc1) ** 2) + ((yc2 - yc1) ** 2) + 1e-7
+    d = ((x_center - x_center_g) ** 2) + ((y_center - y_center_g) ** 2)
+    u = d / c
+    v = torch.atan(w_gt / h_gt) - torch.atan(w_pred / h_pred)
+
+    with torch.no_grad():
+        w_pred1 = w_pred * v
+        h_pred1 = h_pred * v
+
+    ciou_loss =(1 - iouk + u + 8 / (math.pi**2) * (w_pred1 * h_pred - h_pred1 * w_pred)).sum()
+    # v = (4 / (math.pi ** 2)) * torch.pow((torch.atan(w_gt/h_gt)-torch.atan(w_pred/h_pred)),2)
+    # with torch.no_grad():
+    #     S = 1 - iouk
+    #     alpha = v / (S + v)
+    # ciouk = iouk - (u + alpha * v)
+    # iou_weights = bbox_inside_weights.view(-1, 4).mean(1) * bbox_outside_weights.view(-1, 4).mean(1)
+    iouk = (1 - iouk).sum(0) / output.size(0)
+    # ciouk = (1 - ciouk).sum(0) / output.size(0)
+
+    # return iouk, ciouk
+    return iouk, ciou_loss
